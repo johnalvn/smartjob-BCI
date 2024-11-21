9e0e65a36c417c93fbe59c7160b78d226fe105cb
John Alvarez <jean1052@gmail.com>
2024-11-21 06:56:44 -0500

    Basic Java Classes

---
+++ src/main/java/cl/bci/bciapi/BciApiApplication.java
@@ -0,0 +1,13 @@
+package cl.bci.bciapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BciApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BciApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/bci/bciapi/BciApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.bci.bciapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BciApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
